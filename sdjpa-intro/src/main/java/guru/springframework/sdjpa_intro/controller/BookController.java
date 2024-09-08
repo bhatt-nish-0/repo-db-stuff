b55c1e81f71b67e8b4946c0561def824593211bb
bhatt-nish-0 <bhattnish0@gmail.com>
2024-09-08 16:46:05 +0800

    jgnu

--- sdjpa-intro/src/main/java/guru/springframework/sdjpa_intro/controller/BookController.java
+++ sdjpa-intro/src/main/java/guru/springframework/sdjpa_intro/controller/BookController.java
@@ -3,6 +3,8 @@ package guru.springframework.sdjpa_intro.controller;
 import guru.springframework.sdjpa_intro.domain.Book;
 import guru.springframework.sdjpa_intro.repositories.BookRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,5 +27,42 @@ public class BookController {
         System.out.println("hitting this api!");
         System.out.println("hitting this api!");
         return bookRepository.findAll();
+    }
+
+    @PostMapping("save")
+    public void save(@RequestBody BookDto bookDto) {
+        System.out.println("kachre jaise paisa");
+        Book book = new Book(bookDto.getTitle(),bookDto.getIsbn(),bookDto.getPublisher());
+        bookRepository.save(book);
+    }
+}
+
+class BookDto {
+    private String title;
+    private String isbn;
+    private String publisher;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
     }
 }
